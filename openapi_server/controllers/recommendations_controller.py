ad65b895ae2057d2c72d283e2715dda3a1eec011
DavidAlonsoUnex <jalonsoiq@alumnos.unex.es>
2025-11-17 19:24:09 +0100

    HU7: Implementación registro de reproducciones y likes (Simulación)

--- openapi_server/controllers/recommendations_controller.py
+++ openapi_server/controllers/recommendations_controller.py
@@ -1,113 +1,61 @@
 import connexion
-from typing import Dict
-from typing import Tuple
-from typing import Union
+from typing import Dict, Tuple, Union, List
 
-from openapi_server.models.like import Like  # noqa: E501
-from openapi_server.models.play import Play  # noqa: E501
-from openapi_server.models.track import Track  # noqa: E501
+from openapi_server.models.like import Like
+from openapi_server.models.play import Play
+from openapi_server.models.track import Track
 from openapi_server import util
 
+# ==============================================================================
+#  BASE DE DATOS TEMPORAL (Paso 1)
+# ==============================================================================
+_PLAYS_DB = []
+_LIKES_DB = []
 
-def get_artist_plays(id_artist):  # noqa: E501
-    """Obtener reproducciones por artista
+# ==============================================================================
+#  ESCRITURA (PLAYS & LIKES)
+# ==============================================================================
 
-    Devuelve las reproducciones asociadas a un artista. # noqa: E501
+def add_play(body):
+    """Registrar una reproducción"""
+    if connexion.request.is_json:
+        play_obj = body 
+        # TODO: MongoDB implementation pending
+        # db.plays.insert_one(play_obj.to_dict())
+        
+        _PLAYS_DB.append(play_obj)
+        print(f"--> Play registrada: User {play_obj.user_id}")
+    return "Play registered", 201
 
-    :param id_artist: 
-    :type id_artist: str
+def add_like(body):
+    """Registrar un like"""
+    if connexion.request.is_json:
+        like_obj = body
+        # TODO: MongoDB implementation pending
+        # db.likes.insert_one(like_obj.to_dict())
+        
+        _LIKES_DB.append(like_obj)
+        print(f"--> Like registrado: User {like_obj.user_id}")
+    return "Like registered", 201
 
-    :rtype: Union[List[Play], Tuple[List[Play], int], Tuple[List[Play], int, Dict[str, str]]
-    """
-    return 'do some magic!'
+# ==============================================================================
+#  LECTURA BÁSICA
+# ==============================================================================
 
+def get_user_plays(id_user):
+    return [p for p in _PLAYS_DB if p.user_id == id_user]
 
-def get_artist_top_tracks(id_artist):  # noqa: E501
-    """Canciones más escuchadas por artista
+def get_user_likes(id_user):
+    return [l for l in _LIKES_DB if l.user_id == id_user]
 
-    Devuelve las canciones más escuchadas de un artista. # noqa: E501
+def get_track_plays(id_track):
+    return [p for p in _PLAYS_DB if p.track_id == id_track]
 
-    :param id_artist: 
-    :type id_artist: str
+def get_track_likes(id_track):
+    return [l for l in _LIKES_DB if l.track_id == id_track]
 
-    :rtype: Union[List[Track], Tuple[List[Track], int], Tuple[List[Track], int, Dict[str, str]]
-    """
-    return 'do some magic!'
-
-
-def get_recommended_tracks(id_user, type=None):  # noqa: E501
-    """Consultar canciones recomendadas
-
-    Devuelve canciones recomendadas para el usuario filtradas por like, género o suscripción. # noqa: E501
-
-    :param id_user: 
-    :type id_user: str
-    :param type: 
-    :type type: str
-
-    :rtype: Union[List[Track], Tuple[List[Track], int], Tuple[List[Track], int, Dict[str, str]]
-    """
-    return 'do some magic!'
-
-
-def get_top_tracks():  # noqa: E501
-    """Canciones más escuchadas
-
-    Devuelve el listado de canciones más escuchadas. # noqa: E501
-
-
-    :rtype: Union[List[Track], Tuple[List[Track], int], Tuple[List[Track], int, Dict[str, str]]
-    """
-    return 'do some magic!'
-
-
-def get_track_likes(id_track):  # noqa: E501
-    """Obtener likes por canción
-
-    Devuelve los likes de una canción. # noqa: E501
-
-    :param id_track: 
-    :type id_track: str
-
-    :rtype: Union[List[Like], Tuple[List[Like], int], Tuple[List[Like], int, Dict[str, str]]
-    """
-    return 'do some magic!'
-
-
-def get_track_plays(id_track):  # noqa: E501
-    """Obtener reproducciones por canción
-
-    Devuelve las reproducciones de una canción. # noqa: E501
-
-    :param id_track: 
-    :type id_track: str
-
-    :rtype: Union[List[Play], Tuple[List[Play], int], Tuple[List[Play], int, Dict[str, str]]
-    """
-    return 'do some magic!'
-
-
-def get_user_likes(id_user):  # noqa: E501
-    """Obtener likes de usuario
-
-    Devuelve los likes del usuario. # noqa: E501
-
-    :param id_user: 
-    :type id_user: str
-
-    :rtype: Union[List[Like], Tuple[List[Like], int], Tuple[List[Like], int, Dict[str, str]]
-    """
-    return 'do some magic!'
-
-
-def get_user_plays(id_user):  # noqa: E501
-    """Obtener reproducciones de usuario
-
-    Devuelve las reproducciones del usuario. # noqa: E501
-
-    :param id_user: 
-    :type id_user: str
-
-    :rtype: Union[List[Play], Tuple[List[Play], int], Tuple[List[Play], int, Dict[str, str]]
-    """
-    return 'do some magic!'
+# STUBS (Resto vacío por ahora)
+def get_artist_plays(id_artist): return []
+def get_artist_top_tracks(id_artist): return []
+def get_recommended_tracks(id_user, type=None): return []
+def get_top_tracks(): return []

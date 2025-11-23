b803c03d4dc5437c0534bb3c95a5d92143e09b0a
jamarcostt <jamarcost@alumnos.unex.es>
2025-11-23 22:32:32 +0100

    Añadidoconfiguración CORS

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 import connexion
+from flask_cors import CORS
 
 from openapi_server import encoder
 
 
 def main():
     app = connexion.App(__name__, specification_dir='./openapi/')
+    # Habilitar CORS para todas las rutas y orígenes.
+    CORS(app.app)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
                 arguments={'title': 'Microservicio Recomendaciones'},
